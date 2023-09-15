1304078cec269f2ab1daf6d611a7b45f823293e3
Andrei Rivera <andreirive2003@gmail.com>
2023-09-14 19:35:39 -0600

    Router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
+import { routes } from './router/router';
+import React from 'react';
 
-function App() {
+export default function App() {
   return (
-    <>
     <Routes>
-      <Route path="/"/>
+      {routes.map((route, index) => (
+        <Route path={route.path} element={route.element} key={index}/>
+      ))}
     </Routes>
-    </>
   );
 }
-
-export default App;
