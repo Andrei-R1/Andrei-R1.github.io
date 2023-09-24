824cd905c17888a7c0839481051f8ebd0f7c857e
Andrei Rivera <andreirive2003@gmail.com>
2023-09-23 20:56:17 -0600

    Projects page v2

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,34 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import '../styles/Projects.css'
+import githubD from '../assets/dark-mode/github.svg'
+import githubL from '../assets/light-mode/github.svg'
 import Header from '../components/Header'
 import Box from '@mui/material/Box'
 import { useThemeContext } from '../components/ThemeContext'
 
-
-function Projects(){
+function Projects() {
   const { mode } = useThemeContext()
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState([])
 
   useEffect(() => {
-    fetch('projects.json',{
-      headers : { 
+    fetch('projects.json', {
+      headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    })   
+        Accept: 'application/json',
+      },
+    })
       .then((response) => response.json())
       .then((data) => setProjects(data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
+      .catch((error) => console.error('Error fetching data:', error))
+  }, [])
   return (
     <Box sx={{ bgcolor: 'background.default', color: 'text.primary' }}>
-      <Header/>
+      <Header />
       <div className="projects-list">
         {projects.map((project) => (
           <div key={project.id} className="project">
-            <h2>{project.title}</h2>
-            <p>{project.description}</p>
-            <img src={project.image} alt={project.title}/>
+            <div className="left-box">
+              <img src={project.image} alt={project.title} />
+            </div>
+            <div className="right-box">
+              <h1 className='p1'>{project.title}</h1>
+              <h2 className='p2'>{project.description}</h2>
+              <h2 className='p3'>{project.date}</h2>
+              <div className="link">
+                <h2>{'Link= ' + project.url}</h2>
+                <img
+                  src={mode === 'dark' ? githubD : githubL}
+                  alt="GitHub"
+                  onClick={() => {
+                    window.open(project.url, '_blank')
+                  }}
+                />
+              </div>
+            </div>
           </div>
         ))}
       </div>
