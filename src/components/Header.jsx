53f39c30021d1f8a1cebba35fb17a6a9d59a144c
Andrei Rivera <andreirive2003@gmail.com>
2023-09-24 21:04:51 -0600

    Header rework final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,14 +9,30 @@ import gmailL from '../assets/light-mode/gmail.svg'
 import light from '../assets/dark-mode/light.svg'
 import dark from '../assets/light-mode/dark.svg'
 import { useThemeContext } from './ThemeContext'
-import { AppBar, Toolbar, Typography, IconButton } from '@mui/material'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
+import Container from '@mui/material/Container'
+import Button from '@mui/material/Button'
+import MenuItem from '@mui/material/MenuItem'
 
-
-export default function Headerv2() {
+export default function Header() {
   const { mode, toggleMode } = useThemeContext()
+  const [anchorElNav, setAnchorElNav] = React.useState(null)
+
   const handleThemeToggle = () => {
     toggleMode()
     document.body.setAttribute('data-theme', mode)
+  }
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget)
+  }
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null)
   }
   const GmailButton = () => {
     window.open('mailto:andreirive2003@gmail.com', '_blank')
@@ -29,36 +45,175 @@ export default function Headerv2() {
   }
 
   return (
-    <AppBar position="static">
-      <Toolbar sx={{ bgcolor: 'background.default', color: 'text.primary', display: 'flex'}}>
-        <Typography variant="h6" component="div">
-          <a href="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-            HOME
-          </a>
-        </Typography>
-        <Typography variant="h6" component="div">
-          <a href="/projects" style={{ textDecoration: 'none', color: 'inherit' }}>
-            PROJECTS
-          </a>
-        </Typography>
-        <Typography variant="h6" component="div">
-          <a href="/contact" style={{ textDecoration: 'none', color: 'inherit' }}>
-            CONTACT
-          </a>
-        </Typography>
-        <IconButton onClick={GmailButton}>
-          <img src={mode === 'dark' ? gmailD : gmailL} alt="" />
-        </IconButton>
-        <IconButton onClick={LinkedInButton}>
-          <img src={mode === 'dark' ? linkedinD : linkedinL} alt="" />
-        </IconButton>
-        <IconButton onClick={GitHubButton}>
-          <img src={mode === 'dark' ? githubD : githubL} alt="" />
-        </IconButton>
-        <IconButton onClick={handleThemeToggle}>
-          <img src={mode === 'dark' ? light : dark} alt="" />
-        </IconButton>
-      </Toolbar>
+    <AppBar className="header" position="static" sx={{boxShadow:'none'}}>
+      <Container maxWidth="x1" sx={{ bgcolor: 'background.default', color: 'text.primary' }}>
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }}}>
+            <IconButton
+              size="large"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit">
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              keepMounted
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component="a"
+                  href="/"
+                  sx={{
+                    fontFamily: 'Inter',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'text.primary',
+                    textDecoration: 'none',
+                  }}>
+                  HOME
+                </Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component="a"
+                  href="/projects"
+                  sx={{
+                    fontFamily: 'Inter',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'text.primary',
+                    textDecoration: 'none',
+                  }}>
+                  PROJECTS
+                </Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component="a"
+                  href="/contact"
+                  sx={{
+                    fontFamily: 'Inter',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'text.primary',
+                    textDecoration: 'none',
+                  }}>
+                  CONTACT
+                </Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <Box classname="left-header" sx={{ paddingLeft:5, flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Button>
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  fontFamily: 'Inter',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'text.primary',
+                  textDecoration: 'none',
+                  mr: 5,
+                  ml: 5,
+                }}>
+                HOME
+              </Typography>
+            </Button>
+            <Typography
+              variant="h6"
+              noWrap
+              component="p"
+              sx={{
+                fontFamily: 'Inter',
+                fontWeight: 500,
+                fontSize: '1.7rem',
+                color: 'text.primary',
+                alignSelf: 'center',
+              }}>
+              |
+            </Typography>
+            <Button>
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/projects"
+                sx={{
+                  fontFamily: 'Inter',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'text.primary',
+                  textDecoration: 'none',
+                  mr: 5,
+                  ml: 5,
+                }}>
+                PROJECTS
+              </Typography>
+            </Button>
+            <Typography
+              variant="h6"
+              noWrap
+              component="p"
+              sx={{
+                fontFamily: 'Inter',
+                fontWeight: 500,
+                fontSize: '1.7rem',
+                color: 'text.primary',
+                alignSelf: 'center',
+              }}>
+              |
+            </Typography>
+            <Button>
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/contact"
+                sx={{
+                  fontFamily: 'Inter',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'text.primary',
+                  textDecoration: 'none',
+                  mr: 5,
+                  ml: 5,
+                }}>
+                CONTACT
+              </Typography>
+            </Button>
+          </Box>
+          <Box classname="right-header" sx={{ paddingRight:5, display: { xs: 'flex', md: 'flex' }}}>
+            <IconButton onClick={GmailButton} sx={{mr:2, ml:2}}>
+              <img src={mode === 'dark' ? gmailD : gmailL} alt=""/>
+            </IconButton>
+            <IconButton onClick={LinkedInButton} sx={{mr:2, ml:2}}>
+              <img src={mode === 'dark' ? linkedinD : linkedinL} alt="" />
+            </IconButton>
+            <IconButton onClick={GitHubButton} sx={{mr:2, ml:2}}>
+              <img src={mode === 'dark' ? githubD : githubL} alt="" />
+            </IconButton>
+            <IconButton onClick={handleThemeToggle} sx={{mr:2, ml:2}}>
+              <img src={mode === 'dark' ? light : dark} alt="" />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   )
 }
